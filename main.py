922697edb1556375bfc714234c54ee591ef73e29
Frederik <Frederik.kraglund@gmail.com>
2019-03-03 14:01:46 +0100

    main.py, procfile, requirements.txt,runtime.txt

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+# just serve all the static files under root
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+
+# for / root, return Hello Word
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+
+@app.route("/html")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/cv")
+def cv():
+    return render_template("cv.html")
+
+
+@app.route("/motivation")
+def motivation():
+    return render_template("motivation.html")
+
+
+@app.route("/fremtidsplaner")
+def fremtidsplaner():
+    return render_template("fremtidsplaner.html")
+
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
+
+# start listening
+if __name__ == "__main__":
+    app.run(debug=True, port='8080', host='0.0.0.0')
+
